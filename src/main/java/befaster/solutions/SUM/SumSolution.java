44dc611e5e94e67e3f8d34ddf8c73cf3777a74ec
sbx_user1060 <sbx_user1060@ip-10-128-17-86.eu-west-2.compute.internal>
2018-11-25 22:56:26 +0000

    Sun Nov 25 22:56:26 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -3,6 +3,12 @@ package befaster.solutions.SUM;
 public class SumSolution {
 
     public int compute(int x, int y) {
+    	if (x<0 || x>100) {
+    		throw new IllegalArgumentException();
+    	}
+    	if (y<0 || y>100) {
+    		throw new IllegalArgumentException();
+    	}
     	return x+y;
     }
 
