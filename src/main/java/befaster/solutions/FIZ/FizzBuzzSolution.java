105a2e4c50e5ca101ef157675f639c33ae9f8455
sbx_user1060 <sbx_user1060@ip-10-128-17-86.eu-west-2.compute.internal>
2018-11-26 00:18:04 +0000

    Mon Nov 26 00:18:04 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,52 +4,26 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
     
-    	if((number%3==0) || (number.toString().contains("3"))){
-    		if((number%5==0) || (number.toString().contains("5"))){
-    			if (isIdentical(number) && (number > 10)) {
-    				if(number%2 !=0) {
-    					return "fizz buzz fake deluxe";
-    				}
-    				return "fizz buzz deluxe";
+    	if((number%3==0) && (number.toString().contains("3"))) {
+    		if((number%5==0) && (number.toString().contains("5"))) {
+    			if(number%2 != 0) {
+    				return "fizz buzz fake deluxe";
     			}
-    			return "fizz buzz";
-    		} else {
-    			if (isIdentical(number) && (number > 10)) {
-    				if(number%2 !=0) {
-    					return "fizz fake deluxe";
-    				}
-    				return "fizz deluxe";
-    			}
-    			return "fizz";
+    			return "fizz buzz deluxe";
     		}
+    		if(number%2 != 0) {
+    			return "fizz fake deluxe";
+    		}
+    		return "fizz deluxe";
+    	} else if((number%5==0) && (number.toString().contains("5"))) {
+    		if(number%2 != 0) {
+				return "buzz fake deluxe";
+			}
+			return "buzz deluxe"; 		
     	}
-    		else if ((number%5==0) || (number.toString().contains("5"))){
-    			if (isIdentical(number) && (number > 10)) {
-    				if(number%2 !=0) {
-    					return "buzz fake deluxe";
-    				}
-    				return "buzz deluxe";
-    			}
-    			return "buzz";
-    		} else if(isIdentical(number) && (number > 10)){
-    			if(number%2 !=0) {
-					return "fake deluxe";
-				}
-    			return "deluxe";
-    		} else
-        		return Integer.toString(number);
     	
+    	    	
     }
     
-    public boolean isIdentical(Integer num) {
-    	String s = Integer.toString(num);
-    	char[] s1 = s.toCharArray();
-    	int len = s.length();
-    	for(int i =1; i<len; i++) {
-    		if(s1[i] != s1[0]) {
-    			return false;
-    		}
-    	}
-    	return true;
-    }
+
 }
