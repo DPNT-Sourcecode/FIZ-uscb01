041e549334131d1ad10b2c02e110b22c62f64c36
sbx_user1060 <sbx_user1060@ip-10-128-17-86.eu-west-2.compute.internal>
2018-11-26 01:03:18 +0000

    Mon Nov 26 01:03:18 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,43 +4,17 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
     
-    	if((number%3==0) && (number%5==0)) {
-    		if((number.toString().contains("3")) && (number.toString().contains("5"))) {
-    			if(number%2 != 0) {
-    				return "fizz buzz fake deluxe";
-    			}
-    			return "fizz buzz deluxe";
-    		}
-    		return "fizz buzz";
-    	} else if((number%5==0) && (number.toString().contains("5"))) {
-    		if(number%2 != 0) {
-				return "buzz fake deluxe";
-			}
-			return "buzz deluxe"; 		
-    	} else if((number%3==0) || (number.toString().contains("3"))) {
-    		if((number%5==0) || (number.toString().contains("5"))) {
-    			return "fizz buzz";
-    		} else if((number%3==0) && (number.toString().contains("3"))) {
-    			if(number%2 != 0) {
-    				return "fizz fake deluxe";
-    			}
-				return "fizz deluxe";
-			}
-    		return "fizz";
-    	} else if((number%5==0) || (number.toString().contains("5"))) {
-    		if((number%5==0) && (number.toString().contains("5"))) {
-    			if(number%2 != 0) {
-    				return "buzz fake deluxe";
-    			}
-				return "buzz deluxe";
-			}
-    		return "buzz";
-    	} else {
-    		return Integer.toString(number);
-    	}
-    	
-    	    	
+    
     }
     
-
+    public String fizzbuzz(Integer num) {
+    	if((num%3==0)&&(num%5==0))  {
+    		return "fizz buzz";
+    	} else if(num%3 == 0) {
+    		return "fizz";
+    	} else if(num%5 ==0) {
+    		return "buzz";
+    	} else 
+    		return Integer.toString(num);
+    }
 }
